a53d47ee980de4366a6e5f62ec44a2946b245a57
matheusjean28 <matheusjean28@gmail.com>
2023-10-14 18:04:28 -0300

    add picture user

--- src/Contents/Header.jsx
+++ src/Contents/Header.jsx
@@ -1,10 +1,11 @@
-
+import './ContentsStyles/HeaderStyles.css'
 
 export default function headers() {
     return (
         <>
-            <header>
+            <header className='headersConteinerStyles'>
                 <h4>Mac Find History</h4>
+                <img src="public\profile.png"  className="userPic"/>
             </header>
         </>)
 }
