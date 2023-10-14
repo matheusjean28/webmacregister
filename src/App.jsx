c410ae6a6140bff0544deb98d6551fd8a4698189
matheusjean28 <matheusjean28@gmail.com>
2023-10-14 15:06:32 -0300

    styling header of maclist

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Headers from './Contents/Header'
 import SearchBar from './Contents/SearchBar'
 import MacList from './Contents/MacList'
+import HeaderMacList from './Contents/HeaderMacList'
 
 function App() {
 
   return (
     <>
-    <Headers/>
-    <SearchBar />
-    <MacList/>
+      <Headers />
+      <SearchBar />
+      <HeaderMacList />
+      <MacList />
     </>
   )
 }
---
+++ src/Contents/HeaderMacList.jsx
@@ -0,0 +1,14 @@
+
+import './ContentsStyles/HeaderMacListConteinerStyles.css'
+export default function HeaderMacList() {
+    return (
+        <>
+            <div className="HeaderMacListConteiner">
+                <p>MODEL</p>
+                <p>MAC</p>
+                <p>PROBLEM</p>
+                <p>REMOTE ACESS</p>
+            </div>
+        </>
+    )
+}
