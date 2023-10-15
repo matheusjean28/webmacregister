c6886c09f49282118ba009fac769b591d838b37f
matheusjean28 <matheusjean28@gmail.com>
2023-10-15 15:14:43 -0300

    login screen was made, simple

--- src/Contents/UserLogin.jsx
+++ src/Contents/UserLogin.jsx
@@ -1,10 +1,27 @@
 import './ContentsStyles/UserLoginStyles.css'
+import { useState } from 'react';
+export default function UserLogin() {
+    let [loop, setLoop] = useState(false);
 
-export default function UserLogin(){
+    const handleLoop = () => {
+        setLoop(!loop)
+    }
     return (
         <>
             <div className="UserLoginConteiner">
-                <p>start section</p>
+                <h1>START SECTION</h1>
+                <p>You need to be logged to start</p>
+                <form className='LogginUser' action="LogginUser" method="post">
+                    <input type="text" placeholder='E-mail' />
+                    <input type="password" placeholder='Password' />
+                    <button type="submit" onClick={(e) => {
+                        e.preventDefault();
+                        handleLoop()
+                    }} >
+                        Login
+                    </button>
+                    {loop ? <div class="loading" ></div> : ""}
+                </form>
             </div>
         </>
     )
