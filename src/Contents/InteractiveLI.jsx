e815a7abf4f3b40e1fb006af1a5a7ceffec7e40c
matheusjean28 <matheusjean28@gmail.com>
2023-11-11 16:45:38 -0300

    list icon to yes or not remote acess

--- src/Contents/InteractiveLI.jsx
+++ src/Contents/InteractiveLI.jsx
@@ -1,16 +1,33 @@
 import "./ContentsStyles/InteractiveLIStyles.css";
 
 export default function InteractiveLI({ selectLiId, macs }) {
-    console.log(selectLiId);
-    console.log(macs[selectLiId]);
+  console.log(selectLiId);
+  console.log(macs[selectLiId]);
 
-    return (
-        <>
-            <div className="InteractiveLIConteiner">
-                <h3>{macs[selectLiId].model }</h3>
-                <h3>{macs[selectLiId].mac }</h3>
-                <h3>{macs[selectLiId].problem }</h3>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="InteractiveLIConteiner">
+        <h3>Device Informations</h3>
+        <div className="conteinerInfo">
+          <h5 className="InteractiveListH5">Model</h5>
+          <h6 className="InteractiveListH6">{macs[selectLiId].model}</h6>
+
+          <h5 className="InteractiveListH5">MAC</h5>
+          <h6 className="InteractiveListH6">testing</h6>
+
+          <h5 className="InteractiveListH5">PROBLEM</h5>
+          <h6 className="InteractiveListH6">Dont get Connection</h6>
+
+          <h5 className="InteractiveListH5">Remote Acess</h5>
+          <img className="InteractiveListIMG"
+            src={
+              // macs[selectLiId].remoteacess 
+              false
+              ? "public/not.png" : "public/sim.png"
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
 }
