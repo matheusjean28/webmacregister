a85a96045c3f3df3d975120e779bececf42c4434
matheusjean28 <matheusjean28@gmail.com>
2024-05-25 17:06:12 -0300

    search function implementation, return only items that match exactly with the parms searched, others are droped

--- src/Contents/SearchBar.jsx
+++ src/Contents/SearchBar.jsx
@@ -37,7 +37,7 @@ export default function SearchBar() {
   };
 
     const handleSearchMac = () => {
-      SearchResultFilter(find,  ["something"],  )
+      SearchResultFilter(find,  macs,  )
     }
 
   return (
--- src/Utils/SearchResultFilter.jsx
+++ src/Utils/SearchResultFilter.jsx
@@ -1,25 +1,38 @@
-
 /**
  * get user imput and return your search if it exists
  *
  * @param {Array} params - user input.
  * @param {Array} array - Array which contains that data.
  * @param {Function} statesHandle - State handler, UseState-like.
+ * @param {Array} parmsFound array that contains data found at search func
  */
-function SearchResultFilter(params = '', array = [], statesHandle = () => {}) {
+function SearchResultFilter(params = "", array = [], statesHandle = () => {}) {
   console.log("called rigth here!");
 
   try {
-    if(params.length === 0 || array.length === 0){
-        throw new Error ("Params or Array must have data")
+    if (params.length === 0 || array.length === 0) {
+      throw new Error("Params or Array must have data");
     }
 
-    console.log(array)
-    console.log(params)
-  } catch (error) {
-    console.error(error)
-  }
+    var ShouldReturnedValidData = [];
+     array.forEach((item, index) => {
+      item = Object.values(item);
 
+      // check if item is igual to the params searched
+      item.forEach((element) => {
+        if (element === params) {
+          var itemPosition = array[index]
+          ShouldReturnedValidData.push(itemPosition);
+          return ShouldReturnedValidData;
+        }
+      });
+    });
+    
+    // missing set state with this data;
+    return ShouldReturnedValidData;
+  } catch (error) {
+    console.error(error);
+  }
   return;
 }
 
